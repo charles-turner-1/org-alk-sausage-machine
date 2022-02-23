c5ff651abcb9e67a1354ce0c159aa9438fedc8a3
Charles Turner <ct6g18@soton.ac.uk>
2022-02-23 17:01:41 +0000

    Add/update test script

--- test_script_simple.py
+++ test_script_simple.py
@@ -20,16 +20,7 @@ tit.pipeline()
 tit.init_minimiser()
 
 tit.dissociation_consts()
+
+# This is where it goes tits up 
 tit.minimise(1)
 
-# tit = SausageMachine.org_alk_titration("/home/ct/DanKerr")
-# tit.read_excel_spreadsheets("01.09.21.50UM.001_PROCESSED.xlsx",
-#                             "01.09.21.50UM.001.NAOH_PROCESSED.xlsx",
-#                             "01.09.21.50UM.001.BT_PROCESSED.xlsx")
-
-
-# tit.extract_TA_data() # Unsuprisingly, no errors here
-# tit.strip_data("TA")
-# tit.nernst_factor("TA")
-# tit.extract_NaOH_data()
-# tit.vol_to_mass("TA")
