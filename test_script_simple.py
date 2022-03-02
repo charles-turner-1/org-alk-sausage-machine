0f50b5ec0b47efd8cc322447a8f0fadf5177575c
Charles Turner <ct6g18@soton.ac.uk>
2022-03-02 16:36:48 +0000

    Updated test script

--- test_script_simple.py
+++ test_script_simple.py
@@ -16,11 +16,23 @@ tit.read_excel_spreadsheets("01.09.21.50UM.001_PROCESSED.xlsx",
                                        "01.09.21.50UM.001.BT_PROCESSED.xlsx")
 
 tit.pipeline()
-
 tit.init_minimiser()
-
 tit.dissociation_consts()
 
-# This is where it goes tits up 
 tit.minimise(1)
 
+import SausageMachine
+
+tit_simple = SausageMachine.org_alk_titration("~/Python/org-alk-sausage-machine")
+
+tit_simple.read_excel_spreadsheets("01.09.21.50UM.001_PROCESSED.xlsx",
+                                       "01.09.21.50UM.001.NAOH_PROCESSED.xlsx",
+                                       "01.09.21.50UM.001.BT_PROCESSED.xlsx")
+
+tit_simple.pipeline()
+tit_simple.init_minimiser()
+tit_simple.dissociation_consts()
+
+k = tit_simple.minimise_01()
+
+# So where the fuck is the error coming from?
