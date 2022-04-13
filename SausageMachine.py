235f3f5c0bf02ebea1ebd38ba999f0050de1df85
Charles Turner <ct6g18@soton.ac.uk>
2022-04-13 15:08:40 +0100

    Working function to write results to master results file

--- SausageMachine.py
+++ SausageMachine.py
@@ -114,6 +114,7 @@ class org_alk_titration():
         "pK1" : None,
         "pK2" : None,
         "pK3" : None,
+        "CONVERGENCE_FACTOR" : None,
         "pK1_INITIAL" : 4.5,
         "pK2_INITIAL" : 5.25,
         "pK3_INITIAL" : 5.5,
@@ -864,6 +865,7 @@ class org_alk_titration():
         self.df_minimiser_outputs["pK1"][minimiser_no] = -np.log10(self.K_X1)
         self.df_minimiser_outputs["pK2"][minimiser_no] = -np.log10(self.K_X2)
         self.df_minimiser_outputs["pK3"][minimiser_no] = -np.log10(self.K_X3)
+        self.outputs["CONVERGENCE_FACTOR"] = SSR_frac_change_limit
 
 
         if minimiser_no == 1:
@@ -940,9 +942,7 @@ class org_alk_titration():
         df_outputs = pd.read_excel(output_filename)
 
         if sample_filename in df_outputs['SAMPLE'].to_list():
-            raise KeyError("""You are trying to write to a sample name which is already
-                     present in the master results file. Exiting to avoid potentially
-                     overwriting previous results.""")
+            raise KeyError("""You are trying to write to a sample name which is already present in the master results file. Exiting to avoid potentially overwriting previous results.""")
         else:
             self.outputs["SAMPLE"] = sample_filename
         # If we've got this far we now need to create the dataframe that we will 
@@ -955,13 +955,54 @@ class org_alk_titration():
         self.outputs["ORGALK"] = self.titration_features["OA"]["TA_final"] 
 
         self.outputs["MASS"] = "Ask Dan"
+        df_outputs = pd.DataFrame([self.outputs])
 
-        df_outputs.append(self.outputs,ignore_index=True)
+        self.append_df_to_excel(output_filename,df_outputs)
 
-        writer = pd.ExcelWriter(output_filename, engine='openpyxl', mode='a',if_sheet_exists="replace")
-        startrow = writer.book[sheet_name].max_row
+    def append_df_to_excel(self,filename, df, sheet_name='Sheet1', startrow=None,
+                           truncate_sheet=False, 
+                           **to_excel_kwargs):
+        # Excel file doesn't exist - saving and exiting
+        if not os.path.isfile(filename):
+            df.to_excel(
+                filename,
+                sheet_name=sheet_name, 
+                startrow=startrow if startrow is not None else 0, 
+                **to_excel_kwargs)
+            return
 
-        df_outputs.to_excel(writer, sheet_name, startrow=startrow)
+        # ignore [engine] parameter if it was passed
+        if 'engine' in to_excel_kwargs:
+            to_excel_kwargs.pop('engine')
+
+        writer = pd.ExcelWriter(filename, engine='openpyxl', mode='a')
+
+        # try to open an existing workbook
+        writer.book = load_workbook(filename)
+
+        # get the last row in the existing Excel sheet
+        # if it was not specified explicitly
+        if startrow is None and sheet_name in writer.book.sheetnames:
+            startrow = writer.book[sheet_name].max_row
+
+        # truncate sheet
+        if truncate_sheet and sheet_name in writer.book.sheetnames:
+            # index of [sheet_name] sheet
+            idx = writer.book.sheetnames.index(sheet_name)
+            # remove [sheet_name]
+            writer.book.remove(writer.book.worksheets[idx])
+            # create an empty sheet [sheet_name] using old index
+            writer.book.create_sheet(sheet_name, idx)
+
+        # copy existing sheets
+        writer.sheets = {ws.title:ws for ws in writer.book.worksheets}
+
+        if startrow is None:
+            startrow = 0
+
+        # write out the new sheet
+        df.to_excel(writer, sheet_name, startrow=startrow, **to_excel_kwargs)
+
         # save the workbook
         writer.save()
 
